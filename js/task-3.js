13cdd347a068860a3e18d31c7dea4de17cc40fd3
VasylievaKateryna <vasilika0512@gmail.com>
2024-08-30 19:25:59 +0300

    add js

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,18 @@
-function getElementWidth(content, padding, border){
-    
-    
-    return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+function checkForSpam(message) {
+    const blackListWord1 = "spam";
+    const blackListWord2 = "sale";
+if (message.includes(blackListWord1), message.includes(blackListWord2), message.toLowerCase){
+    return true;
+} else  {
+    return false;
+}
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
